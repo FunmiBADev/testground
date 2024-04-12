74aebc9e16252b29b6efa69d271095faf196254c
FunmiBADev <55089250+FunmiBADev@users.noreply.github.com>
2024-04-12 17:38:52 +0100

    Create GemProviderTest.java

---
+++ GemProviderTest.java
@@ -0,0 +1,97 @@
+@Test
+public void testGetName() {
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  String expectedName = "System Variable Credentials";
+  String actualName = provider.getName();
+  assertEquals(expectedName, actualName);
+}
+@Test
+public void testGetVersion() {
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  String expectedVersion = "2.0";
+  String actualVersion = provider.getVersion();
+  assertEquals(expectedVersion, actualVersion);
+}
+
+@Test
+public void testGetDescription() {
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  String expectedDescription = "Example of CredentialsPlugin that takes password from environment variables";
+  String actualDescription = provider.getDescription();
+  assertEquals(expectedDescription, actualDescription);
+}
+
+@Test
+public void testIsSupportedValidCredentials() {
+  String credentials = "username" + SystemVarCredentialsProvider.SPLITTER + "token";
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  boolean supported = provider.isSupported(credentials);
+  assertTrue(supported);
+}
+
+@Test
+public void testIsSupportedInvalidCredentialsNoToken() {
+  String credentials = "username";
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  boolean supported = provider.isSupported(credentials);
+  assertFalse(supported);
+}
+
+@Test
+public void testCreateEditor() {
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  CredentialEditor editor = provider.createEditor();
+  assertTrue(editor instanceof SystemVarCredentialsEditor);
+}
+
+@Test
+public void testResolveCredentialsValid() throws Exception {
+  String credentials = "username" + SystemVarCredentialsProvider.SPLITTER + "token";
+  String host = "localhost";
+  int port = 8080;
+  
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  SystemVarCredentialsEditor mockEditor = Mockito.mock(SystemVarCredentialsEditor.class);
+  Mockito.when(mockEditor.authenticateWithCertificate()).thenReturn("jwtToken");
+  provider.setEditor(mockEditor);
+
+  String resolvedCredentials = provider.resolveCredentials(host, port, credentials);
+  String expectedCredentials = "username:jwtToken";
+  assertEquals(expectedCredentials, resolvedCredentials);
+}
+
+@Test(expected = CredentialsResolvingException.class)
+public void testResolveCredentialsInvalidNoUsername() throws Exception {
+  String credentials = SystemVarCredentialsProvider.SPLITTER + "token";
+  String host = "localhost";
+  int port = 8080;
+  
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  provider.resolveCredentials(host, port, credentials);
+}
+
+//  Test resolveCredentials() method with exception in authenticateWithCertificate: (Mocking required)
+
+// This test requires mocking the authenticateWithCertificate method of SystemVarCredentialsEditor class to throw an exception. Here's an example with Mockito:
+
+@Test(expected = CredentialsResolvingException.class)
+public void testResolveCredentialsException() throws Exception {
+  String credentials = "username" + SystemVarCredentialsProvider.SPLITTER + "token";
+  String host = "localhost";
+  int port = 8080;
+  
+  SystemVarCredentialsProvider provider = new SystemVarCredentialsProvider();
+  SystemVarCredentialsEditor mockEditor = Mockito.mock(SystemVarCredentialsEditor.class);
+  Mockito.when(mockEditor.authenticateWithCertificate()).thenThrow(new RuntimeException("Authentication failed"));
+  provider.setEditor(mockEditor);
+
+  provider.resolveCredentials(host, port, credentials);
+}
+
+@Test
+public void testSplitValidCredentials() throws CredentialsResolvingException {
+  String credentials = "username" + SystemVarCredentialsProvider.SPLITTER + "token";
+  String[] expected = {"username", "token"};
+  String[] actual = SystemVarCredentialsProvider.split(credentials);
+  assertArrayEquals(expected, actual);
+}
