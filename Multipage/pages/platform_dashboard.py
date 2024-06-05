8fdad88cebee159c7ff49a53a85f3d7f6bf0a296
FunmiBADev <55089250+FunmiBADev@users.noreply.github.com>
2024-06-05 11:36:48 +0100

    Update platform_dashboard.py

--- Multipage/pages/platform_dashboard.py
+++ Multipage/pages/platform_dashboard.py
@@ -1,21 +1,20 @@
 import streamlit as st
-import pandas as pd
 
-# Assuming combined_df is imported from app.py
-from app import combined_df, platform_tags
+def display_platform_dashboard(jre_processor):
+    st.title("JRE Versions Dashboard")
 
-st.title("Platform-Specific JRE Versions Dashboard")
+    platform_tag_filter = st.selectbox("Select Platform Tag", options=[tag['tag'] for tag in jre_processor.platform_tags])
 
-platform_tag_filter = st.selectbox("Select Platform Tag", options=[tag['tag'] for tag in platform_tags])
+    filtered_df = jre_processor.combined_df[jre_processor.combined_df['platform_tag'] == platform_tag_filter]
 
-filtered_df = combined_df[combined_df['platform_tag'] == platform_tag_filter]
+    st.write("## Green JREs")
+    green_jres_count = filtered_df['green_jres'].notna().sum()
+    st.write(f"Count: {green_jres_count}")
+    st.dataframe(filtered_df[filtered_df['green_jres'].notna()])
 
-st.write("## Green JREs")
-green_jres_count = filtered_df['green_jres'].notna().sum()
-st.write(f"Count: {green_jres_count}")
-st.dataframe(filtered_df[filtered_df['green_jres'].notna()])
+    st.write("## End-of-Life JREs")
+    eol_jres_count = filtered_df['eol_jres'].notna().sum()
+    st.write(f"Count: {eol_jres_count}")
+    st.dataframe(filtered_df[filtered_df['eol_jres'].notna()])
 
-st.write("## End-of-Life JREs")
-eol_jres_count = filtered_df['eol_jres'].notna().sum()
-st.write(f"Count: {eol_jres_count}")
-st.dataframe(filtered_df[filtered_df['eol_jres'].notna()])
+# Call this function in the main app
